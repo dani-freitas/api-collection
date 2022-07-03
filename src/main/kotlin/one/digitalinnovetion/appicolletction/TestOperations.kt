56607c0ad32829c60eb6cbadcc9c8224c275476a
daniele.freitas <daniele.freitas@ecocentauro.com.br>
2022-07-03 13:41:25 -0300

    TestIntArrayDouble2

--- src/main/kotlin/one/digitalinnovetion/appicolletction/TestOperations.kt
+++ src/main/kotlin/one/digitalinnovetion/appicolletction/TestOperations.kt
@@ -1,7 +1,7 @@
 package one.digitalinnovetion.appicolletction
 
 fun main() {
-    val salario = doubleArrayOf(1500.0,1250.0,5000.0)
+    val salario = doubleArrayOf(2000.0,1250.0,4000.0)
 
     for(valor in salario){
         println(valor)
@@ -18,4 +18,16 @@ fun main() {
          salariosMaiorQue2500.forEach {
             println(it)
         }
+
+    println("-------------------------------------------")
+    println(salario.count { it in 2000.0..5000.0 })
+
+    println("-------------------------------------------")
+    println(salario.find { it == 4000.0 })
+    println(salario.find { it == 3000.0 })
+
+    println("-------------------------------------------")
+    println(salario.any() { it == 4000.0 })
+    println(salario.any() { it == 1000.0 })
+
 }
